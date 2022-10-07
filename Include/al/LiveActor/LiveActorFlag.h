a76f7496f7763d16a303750b05c719effe1b0447
fruityloops1 <fruityloops@littun.co>
2022-10-07 19:28:02 +0200

    add LiveActorFlag

---
+++ Include/al/LiveActor/LiveActorFlag.h
@@ -0,0 +1,19 @@
+#pragma once
+
+namespace al {
+
+struct LiveActorFlag {
+    bool isDead;
+    bool isClipped;
+    bool flag3;
+    bool flag4;
+    bool flag5;
+    bool isHideModel;
+    bool isOffCollide;
+    bool flag8;
+    bool flag9;
+
+    LiveActorFlag();
+};
+
+} // namespace al
---
+++ Source/al/LiveActor/LiveActorFlag.cpp
@@ -0,0 +1,18 @@
+#include "al/LiveActor/LiveActorFlag.h"
+
+namespace al {
+
+LiveActorFlag::LiveActorFlag()
+    : isDead(true)
+    , isClipped(false)
+    , flag3(true)
+    , flag4(false)
+    , flag5(false)
+    , isHideModel(false)
+    , isOffCollide(true)
+    , flag8(false)
+    , flag9(false)
+{
+}
+
+} // namespace al
