48f2a27d73f4b65a4084e93604ffd004ba7ae599
fruityloops1 <fruityloops@littun.co>
2022-10-20 19:21:15 +0200

    fix seadNew warnings

--- Include/sead/basis/seadNew.h
+++ Include/sead/basis/seadNew.h
@@ -2,17 +2,22 @@
 
 #include <new>
 #include <sead/basis/seadTypes.h>
-#include <sead/heap/seadHeap.h>
 #include <stddef.h>
+
+namespace sead {
+
+class Heap;
+
+} // namespace sead
 
 void* operator new(std::size_t size, const std::nothrow_t&) throw();
 void* operator new[](std::size_t size, const std::nothrow_t&) throw();
 
-void* operator new(size_t size, s32 alignment);
-void* operator new[](size_t size, s32 alignment);
+void* operator new(size_t size, s32 alignment) throw();
+void* operator new[](size_t size, s32 alignment) throw();
 
-void* operator new(size_t size, sead::Heap* heap, s32 alignment = sizeof(void*));
-void* operator new[](size_t size, sead::Heap* heap, s32 alignment = sizeof(void*));
+void* operator new(size_t size, sead::Heap* heap, s32 alignment = sizeof(void*)) throw();
+void* operator new[](size_t size, sead::Heap* heap, s32 alignment = sizeof(void*)) throw();
 
 void operator delete(void* ptr, s32);
 void operator delete[](void* ptr, s32);
