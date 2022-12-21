551ce0a86cfc6a54b819060ddd4ef7486ea45f79
hax0kartik <agarwala.kartik@gmail.com>
2022-12-21 19:28:15 +0530

    Implement few svcs

--- Include/nn/svc/svc.h
+++ Include/nn/svc/svc.h
@@ -8,10 +8,11 @@
 
 namespace nn {
 namespace svc {
-
+    __asm nn::Result ControlMemory(u32 *addr, u32 addr0, u32 addr1, u32 size, u32 op, u32 perms);
     __asm void ExitProcess();
     __asm void SleepThread(u64 nanos);
     __asm nn::Result ArbitrateAddress(nn::Handle arbiter, uintptr_t addr, nn::os::ArbitrationType type, s32 value, s64 ns);
+    __asm nn::Result GetThreadPriority(s32* prio, nn::Handle thread);
     __asm nn::Result CloseHandle(nn::Handle handle);
 
     // Debug
--- Source/nn/svc/svc.cpp
+++ Source/nn/svc/svc.cpp
@@ -3,6 +3,19 @@
 namespace nn {
 namespace svc {
 
+    __asm nn::Result __attribute__((section("i._ZN2nn3svc13ControlMemoryEPjjjjjj"))) ControlMemory(u32* addr, u32 addr0, u32 addr1, u32 size, u32 op, u32 perms)
+    {
+        push {r0, r4}
+        ldr r0, [sp, #8]
+        ldr r4, [sp, #0xc]
+        svc 0x1
+        ldr r2, [sp]
+        str r1, [r2]
+        add sp, sp, #4
+        pop {r4}
+        bx lr
+    }
+
     __asm void __attribute__((section("i._ZN2nn3svc11ExitProcessEv"))) ExitProcess()
     {
         svc 0x3
@@ -18,6 +31,16 @@ namespace svc {
     __asm nn::Result __attribute__((section("i._ZN2nn3svc16ArbitrateAddressENS_6HandleEjNS_2os15ArbitrationTypeEix"))) ArbitrateAddress(nn::Handle arbiter, uintptr_t addr, nn::os::ArbitrationType type, s32 value, s64 ns)
     {
         svc 0x22
+        bx lr
+    }
+
+    __asm nn::Result __attribute__((section("i._ZN2nn3svc17GetThreadPriorityEPiNS_6HandleE"))) GetThreadPriority(s32* prio, nn::Handle thread)
+    {
+        push {r0}
+        svc 0xb
+        ldr r2, [sp]
+        str r1, [r2]
+        add sp, sp, #4
         bx lr
     }
 
