e121acb9cae3c172a24532cf9ad7b0c1e93335dd
fruityloops1 <fruityloops@littun.co>
2022-12-22 16:59:31 +0100

    add more misc. functions

--- Include/al/LiveActor/SensorMsg.h
+++ Include/al/LiveActor/SensorMsg.h
@@ -7,13 +7,19 @@ namespace al {
 enum SensorMsg {
     SensorMsg_9 = 9,
     SensorMsg_12 = 12,
+    SensorMsg_14 = 14,
     SensorMsg_16 = 16,
     SensorMsg_FloorTouch = 27,
+    SensorMsg_34 = 34,
+    SensorMsg_37 = 37,
     SensorMsg_40 = 40,
     SensorMsg_EnemyAttack = 45,
+    SensorMsg_49 = 49,
     SensorMsg_50 = 50,
+    SensorMsg_51 = 51,
     SensorMsg_52 = 52,
-    SensorMsg_55 = 55 // something with the KoopaLast door
+    SensorMsg_55 = 55, // something with the KoopaLast door
+    SensorMsg_58 = 58 // something with the KoopaLast door
 };
 
 #define AL_LIVEACTOR_SENSORMSG(TYPE)                    \
