00e56543fa09116851097df088d0facb2e07617b
hax0kartik <agarwala.kartik@gmail.com>
2022-12-22 08:28:37 +0530

    Implement rest of the svcs

--- Source/nn/svc/svc.cpp
+++ Source/nn/svc/svc.cpp
@@ -28,12 +28,6 @@ namespace svc {
         bx lr
     }
 
-    __asm nn::Result __attribute__((section("i._ZN2nn3svc16ArbitrateAddressENS_6HandleEjNS_2os15ArbitrationTypeEix"))) ArbitrateAddress(nn::Handle arbiter, uintptr_t addr, nn::os::ArbitrationType type, s32 value, s64 ns)
-    {
-        svc 0x22
-        bx lr
-    }
-
     __asm nn::Result __attribute__((section("i._ZN2nn3svc17GetThreadPriorityEPiNS_6HandleE"))) GetThreadPriority(s32* prio, nn::Handle thread)
     {
         push {r0}
@@ -44,9 +38,119 @@ namespace svc {
         bx lr
     }
 
+    __asm nn::Result __attribute__((section("i._ZN2nn3svc11CreateMutexEPNS_6HandleEb"))) CreateMutex(nn::Handle* mutex, bool initialLocked)
+    {
+        push {r0}
+        svc 0x13
+        ldr r2, [sp]
+        str r1, [r2]
+        add sp, sp, #4
+        bx lr
+    }
+
+    __asm nn::Result __attribute__((section("i._ZN2nn3svc11CreateEventEPNS_6HandleENS_2os9ResetTypeE"))) CreateEvent(nn::Handle* event, nn::os::ResetType type)
+    {
+        push {r0}
+        svc 0x17
+        ldr r2, [sp]
+        str r1, [r2]
+        add sp, sp, #4
+        bx lr
+    }
+
+    __asm nn::Result __attribute__((section("i._ZN2nn3svc17CreateMemoryBlockEPNS_6HandleEjjjj"))) CreateMemoryBlock(nn::Handle* memblock, u32 addr, u32 size, u32 myperms, u32 otherperms)
+    {
+        push {r0}
+        ldr r0, [sp, #4]
+        svc 0x1e
+        ldr r2, [sp]
+        str r1, [r2]
+        add sp, sp, #4
+        bx lr
+    }
+
+    __asm nn::Result __attribute__((section("i._ZN2nn3svc20CreateAddressArbiterEPNS_6HandleE"))) CreateAddressArbiter(nn::Handle* arbiter)
+    {
+        push {r0}
+        svc 0x21
+        ldr r2, [sp]
+        str r1, [r2]
+        add sp, sp, #4
+        bx lr
+    }
+
+    __asm nn::Result __attribute__((section("i._ZN2nn3svc16ArbitrateAddressENS_6HandleEjNS_2os15ArbitrationTypeEix"))) ArbitrateAddress(nn::Handle arbiter, uintptr_t addr, nn::os::ArbitrationType type, s32 value, s64 ns)
+    {
+        svc 0x22
+        bx lr
+    }
+
     __asm nn::Result __attribute__((section("i._ZN2nn3svc11CloseHandleENS_6HandleE"))) CloseHandle(nn::Handle handle)
     {
         svc 0x23
+        bx lr
+    }
+
+    __asm nn::Result __attribute__((section("i._ZN2nn3svc20WaitSynchronizationNEPiPKNS_6HandleEibx"))) WaitSynchronizationN(int* out, const nn::Handle* handles, s32 handleCount, bool waitAll, s64 timeout_ns)
+    {
+        push {r0, r4}
+        ldr r0, [sp, #4]
+        ldr r4, [sp, #0xC]
+        svc 0x25
+        ldr r2, [sp]
+        str r1, [r2]
+        add sp, sp, #4
+        pop {r4}
+        bx lr
+    }
+
+    __asm nn::Result __attribute__((section("i._ZN2nn3svc15DuplicateHandleEPNS_6HandleES1_"))) DuplicateHandle(nn::Handle* out, nn::Handle original)
+    {
+        push {r0}
+        svc 0x27
+        ldr r2, [sp]
+        str r1, [r2]
+        add sp, sp, #4
+        bx lr
+    }
+
+    __asm nn::Result __attribute__((section("i._ZN2nn3svc13ConnectToPortEPNS_6HandleEPKc"))) ConnectToPort(nn::Handle* port, const char* portName)
+    {
+        push {r0}
+        svc 0x2D
+        ldr r2, [sp]
+        str r1, [r2]
+        add sp, sp, #4
+        bx lr
+    }
+
+    __asm nn::Result __attribute__((section("i._ZN2nn3svc12GetProcessIdEPjNS_6HandleE"))) GetProcessId(unsigned int *id, nn::Handle handle)
+    {
+        push {r0}
+        svc 0x35
+        ldr r2, [sp]
+        str r1, [r2]
+        add sp, sp, #4
+        bx lr
+    }
+
+    __asm nn::Result __attribute__((section("i._ZN2nn3svc11GetThreadIdEPjNS_6HandleE"))) GetThreadId(unsigned int *id, nn::Handle handle)
+    {
+        push {r0}
+        svc 0x37
+        ldr r2, [sp]
+        str r1, [r2]
+        add sp, sp, #4
+        bx lr
+    }
+
+    __asm nn::Result __attribute__((section("i._ZN2nn3svc16GetResourceLimitEPNS_6HandleES1_"))) GetResourceLimit(Handle *resourceLimit, nn::Handle handle)
+    {
+        push {r0}
+        svc 0x38
+        ldr r2, [sp]
+        str r1, [r2]
+        add sp, sp, #4
         bx lr
     }
 
