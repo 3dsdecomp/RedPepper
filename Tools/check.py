9a4a1d85cdd9076a11788c65d4aeac3c111c9ff7
fruityloops1 <fruityloops@littun.co>
2022-12-23 02:41:26 +0100

    update check script for multithreading

--- Tools/check.py
+++ Tools/check.py
@@ -1,5 +1,9 @@
 from diff import *
 from colorama import Fore
+import multiprocessing
+import threading
+import time
+import shutil
 
 def getRankName(rank: str):
     match rank:
@@ -13,40 +17,81 @@ def getRankName(rank: str):
             return "Undecompiled"
     return '?'
 
+def check_file(subdir: str, file: str):
+    filepath = os.path.join(subdir, file)
+    syms = read_sym_file(filepath)
+    newsyms = []
+    for sym in syms:
+        decomp_symbol = get_elf_symbol(sym[0])
+        if (decomp_symbol is None):
+            newsyms.append((sym[0], sym[1], 'U', sym[3], sym[4]))
+            if (sym[2] != 'U'):
+                print(sym[0] + ' ' + getRankName(sym[2]) + ' -> ' + getRankName('U'))
+        else:
+            print(' ' * shutil.get_terminal_size((80, 20)).columns, end='\r')
+            print("Checking " + sym[0], end='\r')
+            rank = rank_symbol(sym, decomp_symbol)
+            newsyms.append((sym[0], sym[1], rank, sym[3], sym[4]))
+            if (sym[2] != rank):
+                print(sym[0] + ' ' + getRankName(sym[2]) + ' -> ' + getRankName(rank))
+    with open(filepath, 'w') as f:
+        for sym in newsyms:
+            f.write(sym[0] + ',' + "{:08x}".format(sym[1]) + ',' + sym[3] + ',' + sym[2])
+            if sym[4] != '':
+                f.write(',' + sym[4] + '\n')
+            else:
+                f.write('\n')
+
+def check_batch(batch: list):
+    for i in range(len(batch)):
+        check_file(batch[i][0], batch[i][1])
+
+class batch_thread(threading.Thread):
+    def __init__(self, batch: list):
+        threading.Thread.__init__(self)
+        self.batch = batch
+    def run(self):
+        check_batch(self.batch)
 
 def main():
+    start = time.time()
+
     checkdir = 'Symbols'
     if len(sys.argv) == 2:
         checkdir = 'Symbols/' + sys.argv[1]
     print('Checking ' + checkdir + '/')
+    
+    sym_files = []
     for subdir, dirs, files in os.walk(checkdir):
         for file in files:
             if "Unnamed.sym" in file:
                 continue
-            filepath = os.path.join(subdir, file)
-            syms = read_sym_file(filepath)
-            newsyms = []
-            for sym in syms:
-                decomp_symbol = get_elf_symbol(sym[0])
-                if (decomp_symbol is None):
-                    newsyms.append((sym[0], sym[1], 'U', sym[3], sym[4]))
-                    if (sym[2] != 'U'):
-                        print(sym[0] + ' ' + getRankName(sym[2]) + ' -> ' + getRankName('U'))
-                else:
-                    print(' ' * 80, end='\r')
-                    print("Checking " + sym[0], end='\r')
-                    rank = rank_symbol(sym, decomp_symbol)
-                    newsyms.append((sym[0], sym[1], rank, sym[3], sym[4]))
-                    if (sym[2] != rank):
-                        print(sym[0] + ' ' + getRankName(sym[2]) + ' -> ' + getRankName(rank))
-            with open(filepath, 'w') as f:
-                for sym in newsyms:
-                    f.write(sym[0] + ',' + "{:08x}".format(sym[1]) + ',' + sym[3] + ',' + sym[2])
-                    if sym[4] != '':
-                        f.write(',' + sym[4] + '\n')
-                    else:
-                        f.write('\n')
-                
+            sym_files.append((subdir, file))
+    
+    file_num = len(sym_files)
+    batch_size = int(file_num / multiprocessing.cpu_count())
+    batches = []
+    cur_batch = []
+    j = 0
+    for i in range(file_num):
+        if j == batch_size:
+            batches.append(cur_batch)
+            cur_batch = []
+            j = 0
+        cur_batch.append(sym_files[i])
+        j += 1
+
+    threads = []
+    for batch in batches:
+        thread = batch_thread(batch)
+        thread.start()
+        threads.append(thread)
+    for thread in threads:
+        thread.join()
+
+    print(' ' * shutil.get_terminal_size((80, 20)).columns, end='\r')
+    print(f"{int(time.time() - start)}s elapsed")
+
 
 if __name__ == "__main__":
     main()
