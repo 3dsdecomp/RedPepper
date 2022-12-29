cbd1a1983cac6f35644dd13f3af3e769acaa764b
hax0kartik <agarwala.kartik@gmail.com>
2022-12-29 09:42:54 +0530

    Add nerves for TrickHintPanel

--- Source/Game/MapObj/TrickHintPanel.cpp
+++ Source/Game/MapObj/TrickHintPanel.cpp
@@ -55,14 +55,32 @@ void TrickHintPanel::exenrv1(){
 
 }
 
+extern "C" int FUN_0026A9B8(u32);
+extern "C" int FUN_0026AA60(u32);
+extern "C" int FUN_002786F4();
+
 void TrickHintPanel::exenrv2(){
-    
+    if(!unk1)
+    {
+        al::startHitReactionStart(this);
+        unk1 = 1;
+    }
+    al::startHitReaction(this, "オン"); // "オン" -> On
+    FUN_0026A9B8(unk0);
+    al::invalidateClipping(this);
+    al::setNerve(this, &NrvTrickHintPanel::nrv3);
 }
 
 void TrickHintPanel::exenrv3(){
-    
+    if(!FUN_002786F4() || al::isGreaterStep(this, 20)){
+        al::setNerve(this, &NrvTrickHintPanel::nrv4);
+    }
 }
 
 void TrickHintPanel::exenrv4(){
-    
+    if(FUN_002786F4()){
+        FUN_0026AA60(unk0);
+        al::validateClipping(this);
+        al::setNerve(this, &NrvTrickHintPanel::nrv1);
+    }
 }
