b6ed61f3dbcbbe38fd9379aa154e5e78847ecd0b
hax0kartik <agarwala.kartik@gmail.com>
2023-01-02 17:38:16 +0530

    Add FlowerPot

---
+++ Include/Game/MapObj/FlowerPot.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "al/MapObj/MapObjActor.h"
+
+class FlowerPot : public al::MapObjActor {
+public:
+    FlowerPot(const sead::SafeString& name);
+
+    virtual void init(const al::ActorInitInfo& info);
+
+    void exeAppear();
+};
