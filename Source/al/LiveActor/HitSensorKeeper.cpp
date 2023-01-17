9b090b38e039c47528ebc3719a283c81371edaa7
hax0kartik <agarwala.kartik@gmail.com>
2023-01-17 21:28:18 +0530

    Fix formatting

--- Source/al/LiveActor/HitSensorKeeper.cpp
+++ Source/al/LiveActor/HitSensorKeeper.cpp
@@ -51,7 +51,7 @@ HitSensor* HitSensorKeeper::getSensor(const char* name) const
     if (mSensors.size() == 1)
         return mSensors.unsafeAt(0);
     
-    for(int i = 0; i < mSensors.size(); i++) {
+    for (int i = 0; i < mSensors.size(); i++) {
         if (al::isEqualString(mSensors.unsafeAt(i)->getName(), name))
             return mSensors.unsafeAt(i);
     }
